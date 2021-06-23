b3e2b00dca083f9fa7b520a92c40f29e4712df8e
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2021-06-23 20:52:16 +0700

    add bootstrap with auth scaffolding

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.user')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">{{ __('Home') }}</div>
+        <div class="card-body">
+          {{ __('User page') }}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/admin', [DashboardController::class, 'index'])->name('dashboard');
+
+Auth::routes();
