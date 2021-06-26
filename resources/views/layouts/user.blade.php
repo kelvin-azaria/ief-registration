8a5f2334ae0475024eaff8c451c57a458b42609f
Ryvel Timtothy Stamber <ryvel.timothy@gmail.com>
2021-06-26 10:09:10 +0700

    menu icon update

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -29,7 +29,7 @@
           <img src="{{ asset('img/site-logo.png') }}" width="232"  alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-          <span class="navbar-toggler-icon"></span>
+          <span class="fas fa-bars text-green"></span>
         </button>
       
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
